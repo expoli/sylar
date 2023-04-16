b9a0312b4eb753d6c6e266b40f359b3fb884653a
expoli <31023767+expoli@users.noreply.github.com>
2023-04-16 12:37:34 +0800

    配置系统02_yaml 1. 怎么样加载一个 yaml、遍历一个 yaml

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -8,6 +8,7 @@
 ********************************************************************************/
 
 #include "../sylar/config.h"
+#include <yaml-cpp/yaml.h>
 
 sylar::ConfigVar<int>::ptr g_int_value_config =
         sylar::Config::Lookup("system.port", (int)8080, "system port");
@@ -15,10 +16,37 @@ sylar::ConfigVar<int>::ptr g_int_value_config =
 sylar::ConfigVar<float>::ptr g_float_value_config =
         sylar::Config::Lookup("system.value", (float)10.2f, "system value");
 
+void print_yaml(const YAML::Node& node, int level) {
+    if (node.IsScalar()) { // 2 是 scalar
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ') << node.Scalar() << " - " << node.Type() << " - " << level;
+    } else if (node.IsNull()) {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ') << "NULL - " << node.Type() << " NULL " << level;
+    } else if (node.IsMap()) { // 2，3 是 map
+        for (auto it = node.begin(); it != node.end(); ++it) {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ') << it->first << " - " << it->second.Type() << " - " << level;
+            print_yaml(it->second, level + 1);
+        }
+    } else if (node.IsSequence()) { // 4 是 sequence
+        for (size_t i = 0; i < node.size(); ++i) {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ') << i << " - " << node[i].Type() << " - " << level;
+            print_yaml(node[i], level + 1);
+        }
+    }
+}
+
+void test_yaml() {
+    YAML::Node root = YAML::LoadFile("/mnt/d/DEV/Clion/sylar_new/bin/conf/log.yaml");
+
+    print_yaml(root, 0);
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root;
+}
+
 int main(int argc, char** argv ) {
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->toString();
 
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_float_value_config->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_float_value_config->toString();
+
+    test_yaml();
 }
